bd5773099211eefb89f0b7409f6a7e72db174922
ttkapostol <ttkapostolaki@gmail.com>
2023-02-23 20:26:53 +0100

    Define work guidelines and create working branch

--- src/js/00main.js
+++ src/js/00main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+// 0- Crear variables básicas
+// 1- Obtener los datos de la API (fetch, lista )
+// 2- Búsqueda por nombre --ev input
+// 3- Incluir/Pintarlos en el HTML : innerHtml / DOM Avanzado
+// 4- Seleccionar los favoritos
+// 5- Pintar los favoritos en su lista (cuidado con los duplicados > comprobar si existen ya o no findIndex)
+// 6- Lista que se queda pintada
+// 7- Lista que se queda guardada en LocalStorage
+// 8- Quitar favoritos de la lista y del localStorage (splice y removeItem?)
+
