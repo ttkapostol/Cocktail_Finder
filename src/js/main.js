9eb5096f60d25bac5fc747784e63c9482e486987
ttkapostol <ttkapostolaki@gmail.com>
2023-02-26 23:47:00 +0100

    Petition to server, fetch and localStorag

--- src/js/main.js
+++ src/js/main.js
@@ -12,4 +12,20 @@ const favouritesList = document.querySelector('.js-list-favourites');
 let cocktailsListData = [];
 let favouritesListData = [];
 
+const storedCocktails = JSON.parse(localStorage.getItem(`cocktails`));
 
+if (storedCocktails) {
+  cocktailsListData = storedCocktails;
+  renderCocktailsList(cocktailsListData);
+  console.log('I have LS');
+} else {
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita') 
+  .then((response) => response.json())
+  .then(data => {
+      cocktailsListData = data.drinks;
+      console.log(data.drinks);
+      renderCocktailsList(cocktailsListData);
+      localStorage.setItem('cocktails', JSON.stringify(cocktailsListData));
+      console.log('Hago fetch');
+  });
+};
