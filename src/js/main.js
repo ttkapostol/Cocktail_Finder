84a85c37e33a6d4995d613d29796da7240a5c433
ttkapostol <ttkapostolaki@gmail.com>
2023-02-26 23:44:53 +0100

    Add basic variables in JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const imgPlaceholder = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+const input = document.querySelector('.js-input');
+const searchBtn = document.querySelector('.js-search-btn');
+const resetBtn = document.querySelector('.js-reset-btn');
+const cocktailsList = document.querySelector('.js-list-cocktails');
+const favouritesList = document.querySelector('.js-list-favourites');
+
+let cocktailsListData = [];
+let favouritesListData = [];
+
+
